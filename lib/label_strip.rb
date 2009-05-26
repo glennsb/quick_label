3df29bf5076c3087dcf938fc3e8bce17d47ed8c6
Stuart B. Glenn <glennsb@lupus.omrf.org>
2009-05-25 21:59:25 -0500

    some basics of to_s, need to test more than 7

--- lib/label_strip.rb
+++ lib/label_strip.rb
@@ -1,18 +1,48 @@
 require 'label'
 
+class Array
+  def / len
+    a = []
+    each_with_index do |x,i|
+      a << [] if i % len == 0
+      a.last << x
+    end
+    a
+  end
+end
+
+
 class LabelStrip
   DEFAULTS = {:per_page => 7,               # number of labels per page.
-              :page_break => "\n\n\n\n\n",  # code for page break.
-              :input_delimeter => '+'
+              :page_break => "\n\n\n\n",  # code for page break.
+              :input_delimeter => '\+',
+              :format => 20
              }.freeze
   
   def initialize(label_input,options = {})
-    append_input(label_input)
+    @labels = []
     @options = DEFAULTS.dup.merge options
+
+    append_input(label_input)
   end
 
   def <<(input)
     append_input(input)
+  end
+
+  
+  # output all label string versions of objects according to options.
+  def to_s
+    @valids = []
+    label_str = []
+    (@labels/@options[:per_page]).each_with_index do |hunk,offset|
+      hunk.each_with_index do |label,index|
+        @valids << label.valid?
+        label_str << label.formatted_label
+      end
+      label_str << @options[:page_break] if 0 == label_str.size % @options[:per_page]
+    end
+    label_str.join("\n")
   end
 
   private
@@ -21,17 +51,8 @@ class LabelStrip
   # Does the work of translating the input text into laabel objects
   #
   def append_input(input)
-    
+    input.split(/\n#{@options[:input_delimeter]}\n/).each do |label_part|
+      @labels << Label.new(label_part,@options[:format])
+    end
   end
-  
-  # # output all label string versions of objects according to options.
-  # def to_s
-  #   labels = []
-  #   in_groups_of options[:per_page] do |objects|
-  #     labels << objects.compact.map do |obj|
-  #       obj.to_label + options[:delimiter]
-  #     end.join
-  #   end
-  #   labels.join options[:page_break]
-  # end
 end
--- test/test_label_strip.rb
+++ test/test_label_strip.rb
@@ -7,6 +7,16 @@ class TestLabelStrip < Test::Unit::TestCase
   {
     :text => "This is a test label\nIt has two lines\n+\nThis is a second lable\nwith two lines two",
   }
+  
+  ACTUAL = "_/3A_/20F
+ This is a test label
+ It has two lines
+-*-
+_/3A_/20F
+ This is a second 
+ lable
+ with two lines two
+-*-"
 
   def setup
     @s = create
@@ -14,6 +24,11 @@ class TestLabelStrip < Test::Unit::TestCase
 
   def test_should_accept_text
     assert_not_nil(@s)
+  end
+  
+  def test_should_produce_labels_string
+    assert_not_nil(@s)
+    assert_equal(@s.to_s, ACTUAL)
   end
   
   private
