8369ab5e47811e8ca3735d6a2614be4293ba54ad
Stuart B. Glenn <glennsb@lupus.omrf.org>
2009-09-24 14:19:27 -0500

    added a bit to support the number of copies, fixes #1873

--- server.rb
+++ server.rb
@@ -12,25 +12,29 @@ end
 
 post "/quick_label*" do
   @strip = LabelStrip.new(params[:labels].gsub(/\r/,"\n"))
+  @copies = params[:copies].to_i
+  if nil == @copies || @copies <= 0 || @copies > 100 then
+    @copies = 1
+  end
   case params[:submit]
     when /validate/
       erb :new
     when /print/
       child = fork do
         if @strip && @strip.valid?
-          print(@strip.to_s)
+          print(@strip.to_s,@copies)
         end
       end
       Process.detach(child)
       erb :new
-#      content_type('text/plain; charset=utf-8')
-#      @strip.to_s
   end
 end
 
-def print(text)
+def print(text,copies=1)
   file = Tempfile.new("quick-label")
-  file.puts(text)
+  copies.times do
+    file.puts(text)
+  end
   file.flush
   file.close
   cmd = "/usr/bin/lpr #{file.path}"
